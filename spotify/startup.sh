8119341fe0a29553f0775baf80ca07386f8c9233
Alexander Schlicker <alexander@schlickster.us>
2018-02-23 21:19:54 -0500

    get spotifyd docker container running

--- spotify/startup.sh
+++ spotify/startup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+# make sure line endings are LF
+/bin/echo "starting dbus"
+/etc/init.d/dbus start
+/bin/echo "starting avahi"
+/etc/init.d/avahi-daemon start
+/bin/echo "starting pulse"
 /usr/bin/pulseaudio -D --system
-/spotifyd/target/release/spotifyd -c /etc/spotifyd/spotifyd.conf
+/bin/echo "running spotifyd"
+/spotifyd/target/release/spotifyd -c /etc/spotifyd/spotifyd.conf --no-daemon
