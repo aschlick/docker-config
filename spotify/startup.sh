28da3b22902af28f1990f8722c20cddc3e7dac37
aschlicker <schlickmeisterj@gmail.com>
2018-03-01 20:28:06 -0500

    working spotify container that ties into server pulseaudio

--- spotify/startup.sh
+++ spotify/startup.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 # make sure line endings are LF
-/bin/echo "starting dbus"
-/etc/init.d/dbus start
-/bin/echo "starting avahi"
-/etc/init.d/avahi-daemon start
-/bin/echo "starting pulse"
-/usr/bin/pulseaudio -D --system
 /bin/echo "running spotifyd"
 /spotifyd/target/release/spotifyd -c /etc/spotifyd/spotifyd.conf --no-daemon
