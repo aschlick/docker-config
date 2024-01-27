08c91d3d07f200b2c0603fd44b645226f4004f90
Alexander Schlicker <alexander@schlickster.us>
2024-01-27 15:17:52 -0500

    remove unused things and update docker configs

--- media-server/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pulseaudio -D --system --disallow-exit
-roc-send -vv -s rtp+rs8m:0.0.0.0:10000 -r rs8m:0.0.0.0:10001 -d pulseaudio
-  
--- spotify/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# make sure line endings are LF
-/bin/echo "running spotifyd"
-/spotifyd/target/release/spotifyd -c /etc/spotifyd/spotifyd.conf --no-daemon
